d5f8e6be16f8fb2944efdd4bfd5c11ca73afc820
ChanSeongKim <cskimair@gmail.com>
2022-03-15 18:00:21 +0900

    image_font_add_app, 1st, change

---
+++ Chapter4/image_font_add_app/android/app/src/main/kotlin/com/editech/image_font_add_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.editech.image_font_add_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Chapter4/image_font_add_app/lib/ImageWidget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ImageWidgetApp extends StatefulWidget{
+  const ImageWidgetApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState(){
+    return _ImageWidgetApp() ;
+  }
+}
+
+class _ImageWidgetApp extends State<ImageWidgetApp>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text ('image Widget'), ),
+      body: Container(
+        child: Center (
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('image/flutter_logo.png', width:200, height:100, fit: BoxFit.fill ),
+              Text('Hello Flutter',
+                style: TextStyle(fontFamily: 'Pacifico',
+                  fontSize: 30, color: Colors.blue )
+              )
+            ],
+          )
+        )
+      )
+    ) ;
+  }
+}
