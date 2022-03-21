7f8c747405176f62e78bbbdb217d240e28953533
ChanSeongKim <cskimair@gmail.com>
2022-03-21 18:22:49 +0900

    largefile_download 1st change(add),2nd,3rd, 4th(~197page) , static url image file download

--- chapter7/largefile_download/lib/largeFileMain.dart
+++ chapter7/largefile_download/lib/largeFileMain.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
-import 'dart:io';
 
 class LargeFileMain extends StatefulWidget{
   const LargeFileMain({Key? key}) : super(key: key);
@@ -24,10 +25,77 @@ class _LargeFileMain extends State<LargeFileMain> {
     //return null ;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Large File Example'),
+        title: const Text('Large File Example'),
       ),
+      body: Center(
+        child: downloading
+            ? Container(
+          height: 120.0,
+          width: 200.0,
+          child:  Card(
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  <Widget>[
+                CircularProgressIndicator(),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Downloading File: $progressString',
+                  style: TextStyle(
+                    color: Colors.white,
+                  )
+                )
+              ],
+            ),
+          ),
+          )
+            :// downloading is false then following code is executed
+              FutureBuilder(
+                builder: (context, snapshot){
+                  switch( snapshot.connectionState){
+                    case ConnectionState.none :
+                      print('none');
+                      return Text('No Data exist');
+                    case ConnectionState.waiting:
+                      print('waiting');
+                      return CircularProgressIndicator();
+                    case ConnectionState.active:
+                      print('active');
+                      return CircularProgressIndicator();
+                    case ConnectionState.done:
+                      print('done');
+                      if( snapshot.hasData){
+                      return snapshot.data as Widget ;
+                      }
+                  }
+                  print('end process');
+                  return Text('no data Exist');
+                },
+                future: downloadWidget( file) ,
+              )
+      )
     );
   }
+
+  Future<Widget> downloadWidget(String filePath) async {
+    File file = File(filePath) ;
+    bool exist = await file.exists() ;
+    //new FileImage(file).evict(); // initialize cache  , new is unnecessary
+    FileImage(file).evict(); // initialize cache
+
+    if( exist){
+      return Center(
+        child: Column(
+          children: <Widget>[Image.file(File(filePath))],
+        )
+      );
+    }else {
+      return const Text('No Data');
+    }
+  }
+
   Future<void> downloadFile() async {
     Dio dio = Dio() ;
     try {
