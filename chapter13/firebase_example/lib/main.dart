ca6ed7c5cdf0da18f46f052535b10bb5e29ea73a
ChanSeongKim <cskimair@gmail.com>
2022-04-28 19:16:45 +0900

    largefile_download 1st change(add),2nd,3rd ~352page, ch13. firebase

--- chapter13/firebase_example/lib/main.dart
+++ chapter13/firebase_example/lib/main.dart
@@ -1,30 +1,33 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+
+
+
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase Example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      navigatorObservers: <NavigatorObserver>[observer],
+      home: const FirebaseApp(
+        analytics: analytics,
+        observer: observer,
+          ),
     );
   }
 }
